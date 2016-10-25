7a6ae2401b695295fe64743284ff82ca53abf5f8
Zhennan <Zhennan@ZHENNANH>
2016-10-25 15:19:54 +0100

    Add project files.

---
+++ OnlineMarking/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineMarking.Startup))]
+namespace OnlineMarking
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
